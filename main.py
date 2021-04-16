2802c9118613f07a9959d4c6821514e9fff54ab7
Dillon <dillon@DN0a224474.SUNet>
2021-04-16 12:49:08 -0700

    Init

---
+++ main.py
@@ -0,0 +1,56 @@
+class Constraint:
+
+    def __init__(self, rc, cc, bc, b, comparator):
+        self.rc = rc
+        self.cc = cc
+        self.bc = bc
+        self.b = b
+        self.comparator = comparator
+
+class Piece:
+
+    def __init__(self, value, constraints):
+        self.value = value
+        self.constraints = constraints
+
+    def contains(self, r, c):
+        return True
+
+    def at(self, r, c):
+        return self.value
+
+class Matrix:
+
+    def __init__(self, name, rows, cols):
+        self.name = name
+        self.pieces = []
+        self.pieces.append(Piece(0, []))
+
+    def add_piece(self, value, constraints):
+        self.pieces[value] = constraints
+
+    def realize(self, values):
+        None
+
+    def at(self, r, c):
+        for piece in self.pieces:
+            if piece.contains(r, c):
+                return piece.at(r, c)
+        assert(False)
+
+    def __repr__(self):
+        return self.name
+
+
+
+# Building a zero matrix
+D = Matrix('D', 'm', 'n')
+print(D)
+
+D.realize({'m' : 10, 'n' : 10})
+
+assert(D.at(0, 0) == 0)
+
+# cc = Constraint(1, 1, 0, 0, '=')
+# A.add_piece('a', [cc])
+
