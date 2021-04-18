e51ff59f170bf3a3094a27f6354024ee0654cac0
Dillon <dillon@DN526av3.SUNet>
2021-04-18 14:45:32 -0700

    Piecewise function works ok, but there is no built-in way to do presburger style truth value decompositions

--- main.py
+++ main.py
@@ -1,74 +1,82 @@
 from sympy import *
 import copy
 
-class Piece:
+# class Piece:
 
-    def __init__(self, f, P):
-        self.f = f
-        self.P = P
+    # def __init__(self, f, P):
+        # self.f = f
+        # self.P = P
 
-    def __repr__(self):
-        return '({0} if {1})'.format(self.f, self.P)
+    # def __repr__(self):
+        # return '({0} if {1})'.format(self.f, self.P)
 
-class PiecewiseExpression:
+# class PiecewiseExpression:
 
-    def __init__(self):
-        self.pieces = []
+    # def __init__(self):
+        # self.pieces = []
 
-    def subs(self, x, y):
-        rp = copy.deepcopy(self)
-        for p in rp.pieces:
-            p.f = p.f.subs(x, y)
-            cs = []
-            for cc in p.P:
-                cs.append(cc.subs(x, y))
-            p.P = cs
-        return rp
+    # def subs(self, x, y):
+        # rp = copy.deepcopy(self)
+        # for p in rp.pieces:
+            # p.f = p.f.subs(x, y)
+            # cs = []
+            # for cc in p.P:
+                # cs.append(cc.subs(x, y))
+            # p.P = cs
+        # return rp
 
-    def add_piece(self, f, p):
-        self.pieces.append(Piece(f, p))
+    # def add_piece(self, f, p):
+        # self.pieces.append(Piece(f, p))
 
-    def __repr__(self):
-        ss = '[{0}]'.format(self.pieces)
-        return ss
+    # def __repr__(self):
+        # ss = '[{0}]'.format(self.pieces)
+        # return ss
 
-def compose_pointwise(op, f, g):
-    composed = PiecewiseExpression()
-    for fp in f.pieces:
-        for gp in g.pieces:
-            composed.pieces.append(Piece(op(fp.f, gp.f), fp.P + gp.P))
-    return composed
+# def compose_pointwise(op, f, g):
+    # composed = []
+    # for fp in f.pieces:
+        # for gp in g.pieces:
+            # composed.pieces.append(Piece(op(fp.f, gp.f), fp.P + gp.P))
+    # return composed
+
+# def pwmul(a, b):
+    # return compose_pointwise(lambda x, y: x*y, a, b)
 
 x, k = symbols("x k")
-pwf = PiecewiseExpression()
-pwf.add_piece(0, [2*k > x])
-pwf.add_piece(1, [2*k <= x])
 
-p0 = PiecewiseExpression()
-p0.add_piece(5, [k > x])
-p0.add_piece(7, [k <= x])
+# pwf = Piecewise()
 
-print(pwf)
+# pwf.add_piece(0, [2*k > x])
+# pwf.add_piece(1, [2*k <= x])
 
-print(compose_pointwise(lambda x, y: x*y, pwf, p0))
+# p0 = PiecewiseExpression()
+# p0.add_piece(5, [k > x])
+# p0.add_piece(7, [k <= x])
+
+# print(pwf)
+
+# print(compose_pointwise(lambda x, y: x*y, pwf, p0))
+
 
 # Build a identity matrix
 r, c = symbols("r c")
 N = symbols("N")
 
-Bnds = [1 <= r, r <= N, 1 <= c, c <= N]
-I = PiecewiseExpression()
+Bnds = And(1 <= r, r <= N, 1 <= c, c <= N)
+print(Bnds)
 
-I.add_piece(Expr(0), Bnds + [r < c])
-I.add_piece(Expr(0), Bnds + [r > c])
-I.add_piece(Expr(1), Bnds + [Eq(r, c)])
+I = Piecewise((nsimplify(0), And(Bnds, r < c)), (nsimplify(0), And(Bnds, r < c)), (nsimplify(1), And(Bnds, Eq(r, c))))
 
 print(I)
 
 Il = I.subs(c, k)
-print('Il: ', Il)
 Ir = I.subs(r, k)
-print('Ir: ', Ir)
 
+print(Il)
+print(Ir)
 
+prod = piecewise_fold(Il*Ir)
+print(prod)
 
+# print(prod.as_expr_set_pairs())
+# print(prod._intervals(k))
