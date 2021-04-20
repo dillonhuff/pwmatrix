e7e187c2749552018a4cbe0c724a2c99890273cd
Dillon <dillon@DN0a232417.SUNet>
2021-04-20 14:33:13 -0700

    Can now do beta reduction

--- language.py
+++ language.py
@@ -1,6 +1,9 @@
 from sympy import *
 import copy
 
+def substitute(target, value, expr):
+    return expr.subs(target, value)
+
 class Lambda:
 
     def __init__(self, vs, f):
@@ -8,7 +11,13 @@ class Lambda:
         self.f = f
 
     def __repr__(self):
-        return "\u03BB {0}. {1}".format(self.vs, self.f)
+        return "(\u03BB {0}. {1})".format(self.vs, self.f)
+
+    def subs(self, target, value):
+        if target in self.vs:
+            return copy.deepcopy(self)
+        print('Doing subs on {}'.format(self))
+        return Lambda(self.vs, substitute(target, value, self.f))
 
 class Set:
 
@@ -19,6 +28,11 @@ class Set:
     def __repr__(self):
         return "{" + str(self.vs) + " | " + str(self.constraints) + "}"
 
+    def subs(self, target, value):
+        if target in self.vs:
+            return copy.deepcopy(self)
+        return Set(self.vs, list(map(lambda x: substitute(target, value, x), self.constraints)))
+
 class App:
 
     def __init__(self, f, vs):
@@ -26,16 +40,21 @@ class App:
         self.vs = vs
 
     def __repr__(self):
-        return '{0}{1}'.format(self.f, self.vs)
+        return '({0}{1})'.format(self.f, self.vs)
+
+    def subs(self, target, value):
+        return App(substitute(target, value, self.f), list(map(lambda x: substitute(target, value, x), self.vs)))
 
 class SymSum:
 
-    def __init__(self, domain, f):
-        self.domain = domain
-        self.f = f
+    def __init__(self):
+        None
 
     def __repr__(self):
-        return '\u2211' + '{' + str(self.domain) + '}' + '({0})'.format(self.f)
+        return '\u2211'
+
+    def subs(self, target, value):
+        return self
 
 class ConcreteSum:
 
@@ -51,15 +70,35 @@ class Op:
         self.name = name
         self.args = args
 
-i0, j0, t= symbols("i0 j0 t")
+def beta_reduce(expr):
+    if isinstance(expr, App):
+        print('Beta reducing {0}'.format(expr))
+        f = expr.f
+        vs = expr.vs
+        if isinstance(f, Lambda):
+            print('Beta reducing lambda {0}'.format(f))
+            fresh = copy.deepcopy(f)
+            for i in range(min(len(vs), len(f.vs))):
+                var = f.vs[i]
+                value = vs[i]
+                fresh = substitute(var, value, f.f)
+            return fresh
+        else:
+            return expr
+    else:
+        return expr
 
-le = Lambda([i0], Set([j0], [1 <= i0, i0 <= j0]))
+i0, j0, t = symbols("i0 j0 t")
+
+le = Lambda([i0], Set([j0], [1 <= j0, j0 <= i0]))
 print(le)
 
 f = Lambda([t], t)
 print(f)
 
 i = symbols("i")
-ss = Lambda([i], SymSum(App(le, [i]), f))
+ss = Lambda([i], App(SymSum(), [App(le, [i]), f]))
 
 print(ss)
+
+print(beta_reduce(App(ss, [7])))
